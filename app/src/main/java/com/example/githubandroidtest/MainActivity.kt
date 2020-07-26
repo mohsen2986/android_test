d114851eed2e755a1f5f38861097701fb9029601
mohsen <mohsenaliakbari.atom@hotmail.com>
2020-07-26 19:50:08 +0430

    toast example

--- app/src/main/java/com/example/githubandroidtest/MainActivity.kt
+++ app/src/main/java/com/example/githubandroidtest/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.githubandroidtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(this , "this is a test" , Toast.LENGTH_SHORT).show()
+
     }
 }
